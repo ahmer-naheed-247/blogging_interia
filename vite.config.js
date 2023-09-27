b807e2fab1ba019630525a93a362e1bbf32a22d4
Ahmer Naheed <ahmer.naheed247@gmail.com>
2023-09-27 14:57:23 +0500

    intertia

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@components': '/resources/js/Components',
+            '@layouts': '/resources/js/Layouts',
+            '@pages': '/resources/js/Pages',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+        
+    },
+});
